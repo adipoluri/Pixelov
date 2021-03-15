57573e691d6272874bb689ed141040c99a479979
adipoluri <adipoluri@gmail.com>
2021-03-14 19:06:16 -0700

    added FireStoreUtils

--- lib/core/authenticationService.dart
+++ lib/core/authenticationService.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class AuthenticationService {
   final FirebaseAuth _firebaseAuth;
@@ -26,4 +28,29 @@ class AuthenticationService {
       return e.message;
     }
   }
+}
+
+class FireStoreUtils {
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Reference storage = FirebaseStorage.instance.ref();
+
+  Future<User> getCurrentUser(String uid) async {
+    DocumentSnapshot userDocument =
+        await firestore.collection('users').doc(uid).get();
+    if (userDocument != null && userDocument.exists) {
+      //return User.fromJson(userDocument.data());
+    } else {
+      return null;
+    }
+  }
+
+  //static Future<User> updateCurrentUser(User user) async {
+  //  return await firestore
+  //      .collection(USERS)
+  //      .doc(user.userID)
+  //      .set(user.toJson())
+  //      .then((document) {
+  //    return user;
+  //  });
+  //}
 }
